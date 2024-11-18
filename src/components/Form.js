bbbea2eb879fe62b801937a6839f4fd945b77f39
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-11-17 17:19:11 -0700

    Replaced form boilerplate with form with text input and a submit button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,11 @@
 export default function Form(props){
     // The component must return some JSX
-    return <h1>The Form Component</h1>;
-  };
+    return (
+        <div>
+          <form>
+            <input type="text" />
+            <input type="submit" value="submit" />
+          </form>
+        </div>
+      );
+    }
