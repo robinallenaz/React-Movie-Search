cc5bd43e9be55eb8fc26c7c95eeb8c3e0fd9b344
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-11-17 17:29:37 -0700

    Updated MovieDisplay component to display data

--- src/components/MovieDisplay.js
+++ src/components/MovieDisplay.js
@@ -1,4 +1,11 @@
-export default function MovieDisplay(props) {
-    // The component must return some JSX
-    return <h1>The MovieDisplay Component</h1>;
-  };
+// You can also destructure your props directly from the parameter list
+export default function MovieDisplay({ movie }) {
+    return (
+      <>
+        <h1>{movie.Title}</h1>
+        <h2>{movie.Genre}</h2>
+        <img src={movie.Poster} alt={movie.Title} />
+        <h2>{movie.Year}</h2>
+      </>
+    );
+  }
