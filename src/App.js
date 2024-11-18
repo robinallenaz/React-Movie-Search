bc9a8ea988f7e39219ff540ce288460975df9913
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-11-17 17:16:46 -0700

    Added boilerplate to components for form and movie display, imported them in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+//Importing our components
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Form />
+      <MovieDisplay />
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
